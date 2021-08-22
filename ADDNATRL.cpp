9ed14e0145752c4103a1c9b9d11a7b4a4a61f2d5
MVRKKSK <82573421+MVRKKSK@users.noreply.github.com>
2021-08-22 18:04:39 +0530

    Add files via upload

---
+++ ADDNATRL.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    long long sum;
+    sum = 0;
+    for(long long i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    cout<<sum;
+   
+    return 0;
+    
+}
