ce701cb34ceaa4ffe435c327230e7a0b537b3845
MVRKKSK <82573421+MVRKKSK@users.noreply.github.com>
2021-10-15 19:56:14 +0530

    Create luckynumber.cpp

---
+++ luckynumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	while(n--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a==7 || b==7 || c==7){
+			cout<<"YES"<<endl;
+		}
+		else cout<<"NO"<<endl;
+	}
+}
